e7ff360e1f7284bb237cd1d4596a149744440ad8
Frank Schmitt <frankschmitt@gmx.de>
2019-11-10 14:05:26 +0100

    Use correct Mustache template for rendering entry list.

--- src/main/kotlin/de/qwhon/justOne/HtmlController.kt
+++ src/main/kotlin/de/qwhon/justOne/HtmlController.kt
@@ -1,5 +1,6 @@
 package de.qwhon.justOne
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
@@ -11,16 +12,21 @@ import java.util.*
 @Controller
 class HtmlController(private val repository: EntryRepository) {
 
+    var logger = LoggerFactory.getLogger(HtmlController::class.java)
+
     @GetMapping("/")
     fun entry(model: Model): String {
         model["title"] = "All entries" // shorthand for model.addAttribute("title", "Blog")
-        model["entries"] = repository.findAllByOrderByNameAsc().map { it.render() }
-        return "justOne" // name of the template we want to use for rendering
+        val allEntries = repository.findAllByOrderByNameAsc()
+        logger.info("found ${allEntries.count()} entries")
+        model["entries"] = allEntries.map { it.render() }
+        return "allEntries" // name of the template we want to use for rendering
     }
 
  }
 
 fun Entry.render() = RenderedEntry(
+        id ?: -1,
         name,
         addedAt.format()
 )
@@ -33,5 +39,6 @@ private fun LocalDateTime.format(): String {
 }
 
 data class RenderedEntry(
+        val id: Long,
         val name: String,
         val addedAt: String)
